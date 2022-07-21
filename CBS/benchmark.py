8b7661aced556f8453cb8be4b1bc6e7811e1f700
Philomena Moek <76845802+Philomena892@users.noreply.github.com>
2022-07-21 15:03:43 +0200

    timeout works now, set to 3 min

--- CBS/benchmark.py
+++ CBS/benchmark.py
@@ -3,14 +3,18 @@
 
 import os
 import signal
+import sys
+from node import main as run
 
 problems = []
 root = "./benchmark_examples/"
 path = os.path.join(root, "targetdirectory")
+print(type(path))
 
 for path, subdirs, files in os.walk(root):
     for name in files:
         problems.append(os.path.join(path, name))
+
 
 print(problems) # contains all filenames in this and subdirectories
 
@@ -24,7 +28,16 @@ def handler(signum, frame):
 # This function *may* run for an indetermined time...
 def run_example(problem, output):
 
-    os.system(f"python node.py -b {problem} {output} > /dev/null 2>&1")
+    
+    stats = problem.split("/")
+    print(stats)
+    
+    size = int(stats[2][-1])
+    print(size)
+
+    # horizon is size*2
+    # > /dev/null 2>&1 to get no output
+    os.system(f"python node.py -b -hz {size*2} {problem} {output}")
     print(f"{problem} successfully run.")
     return
     
@@ -38,12 +51,23 @@ def main():
     for file in problems:
 
         signal.signal(signal.SIGALRM, handler)
-        # timeout after 5 min
-        signal.alarm(5 * 60)
+        # timeout after 3 min
+        signal.alarm(3*60)
 
+        stats = file.split("/")
+        print(stats)
+        
+        size = int(stats[2][-1])
+        print(size)
+        
         try:
-            run_example(file, output)
+
+            # horizon is size*2
+            run(["-b",f"-hz={size*2}",f"{file}",f"{output}"])
+            print(f"{file} successfully run.")
+
         except Exception as exc: 
+        
             print(exc)
             print(f"failed to complete testing on {file}")
         
