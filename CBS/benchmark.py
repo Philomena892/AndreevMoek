51ba91ebb063ca0e81adf2311328d3e9abaa284a
Philomena Moek <76845802+Philomena892@users.noreply.github.com>
2022-07-25 18:11:34 +0200

    Update benchmark.py

--- CBS/benchmark.py
+++ CBS/benchmark.py
@@ -4,13 +4,21 @@
 import os
 import signal
 from socket import timeout
-import sys
+import csv
+import argparse
 from node import main as run
+from node_greedy import main as grun
 
 class TimeOutException(Exception):
     def __init__(self, message='Aborted execution'):
         # Call the base class constructor with the parameters it needs
         super(TimeOutException, self).__init__(message)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g","--greedy", help="enable when you want to use a faster but suboptimal greedy search", action="store_true")
+args = parser.parse_args()
+
 
 
 problems = []
@@ -44,6 +52,12 @@ def main():
         print(stats)
         
         size = int(stats[2][-1])
+        density = int(stats[3][-2])
+        num = int(stats[4].split(".")[0].split("x")[1])
+
+        if size < 8: continue
+        if size == 8 and density < 4: continue
+        if size == 8 and density == 4 and num not in [8,9]: continue
 
         signal.signal(signal.SIGALRM, handler)
         # timeout after 5 min
@@ -52,14 +66,20 @@ def main():
         try:
 
             # horizon is size*2
-            run(["-b",f"-hz={size*2}",f"{file}",f"{output}"])
+            if args.greedy:
+                grun(["-b","-g",f"-hz={size*2}",f"{file}",f"{output}"])
+            else:
+                run(["-b",f"-hz={size*2}",f"{file}",f"{output}"])
             print(f"{file} successfully run.")
 
         except TimeOutException as exc: 
         
             print(exc)
             print(f"failed to complete testing on {file}")
-        
+            with open(output, 'a', encoding='utf-8', newline='') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([f'{file}'])                
+
         signal.alarm(0) # cancel timer if function returns before timeout
 
 if __name__ == "__main__":
