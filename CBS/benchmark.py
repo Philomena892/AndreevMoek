ea4508d3a235e5fac6168660ee707321013be34a
Philomena Moek <76845802+Philomena892@users.noreply.github.com>
2022-07-28 16:29:51 +0200

    does not work on windows

--- CBS/benchmark.py
+++ CBS/benchmark.py
@@ -16,30 +16,6 @@ class TimeOutException(Exception):
         super(TimeOutException, self).__init__(message)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-g","--greedy", help="enable when you want to test the greedy implementation", action="store_true")
-parser.add_argument('directory', nargs='?', type=str, default="./benchmark_examples/", help="By default runs tests on directory '.benchmark_examples/'. Specify a different name here, if you want to benchmark a different directory")
-args = parser.parse_args()
-
-
-
-
-if not os.path.isdir(args.directory):
-    sys.exit(f"{args.directory} is not a directory")
-
-path = os.path.join(args.directory, "targetdirectory")
-print(type(path))
-
-problems = []
-for path, subdirs, files in os.walk(args.directory):
-    for name in files:
-        problems.append(os.path.join(path, name))
-
-
-print(problems) # contains all filenames in this and subdirectories
-problems.sort()
-
-
 # Register a handler for the timeout
 def handler(signum, frame):
     raise TimeOutException()
@@ -47,22 +23,43 @@ def handler(signum, frame):
            
 # Register the signal function handler
 def main():
-    
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g","--greedy", help="enable when you want to test the greedy implementation", action="store_true")
+    parser.add_argument('directory', nargs='?', type=str, default="./benchmark_examples/", help="By default runs tests on directory '.benchmark_examples/'. Specify a different name here, if you want to benchmark a different directory")
+    args = parser.parse_args()
+
+
+    if not os.path.isdir(args.directory):
+        sys.exit(f"{args.directory} is not a directory")
+
+    path = args.directory
+    print(path)
+
+    problems = []
+    for path, subdirs, files in os.walk(args.directory):
+        for name in files:
+            problems.append(os.path.join(path, name))
+
+
+    print(f"\n{problems}") # contains all filenames in this and subdirectories
+    problems.sort()
+    
     output = "benchmark_results.csv"
 
     for file in problems:
 
         stats = file.split("/")
+        print(f"stats: {stats}")
         print(stats)
         
-        size = int(stats[2][-1])
-        density = int(stats[3][-2])
-        num = int(stats[4].split(".")[0].split("x")[1])
+        # size has to manually set if different dir than benchmark_examples
+        if args.directory == "num_robots_benchmark":
+            size = 7
+        else: size = int(stats[2][-1])
+        # density = int(stats[3][-2])
+        # num = int(stats[4].split(".")[0].split("x")[1])
 
-        if size < 8: continue
-        if size == 8 and density < 4: continue
-        if size == 8 and density == 4 and num not in [8,9]: continue
 
         signal.signal(signal.SIGALRM, handler)
         # timeout after 5 min
