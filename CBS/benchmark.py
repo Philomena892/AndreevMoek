b0f5e74bb43a71b7482fce58f135d7d2a4e0afcb
Philomena Moek <76845802+Philomena892@users.noreply.github.com>
2022-07-28 16:52:38 +0200

    now works under windows
    size has to be manually set (line 59) if you are benchmarking on file structure different from benchmark_examples
    timeout does not work on windows

--- CBS/benchmark.py
+++ CBS/benchmark.py
@@ -1,6 +1,9 @@
 # # run all examples from benchmark_examples
 # # with a timelimit of 5 min for each example
 
+# does not work under windows. To develop compatibility check this out: 
+# https://stackoverflow.com/questions/8420422/python-windows-equivalent-of-sigalrm
+
 import os
 import signal
 from socket import timeout
@@ -42,7 +45,7 @@ def main():
             problems.append(os.path.join(path, name))
 
 
-    print(f"\n{problems}") # contains all filenames in this and subdirectories
+    # problems contains all filenames in this and subdirectories
     problems.sort()
     
     output = "benchmark_results.csv"
@@ -50,8 +53,6 @@ def main():
     for file in problems:
 
         stats = file.split("/")
-        print(f"stats: {stats}")
-        print(stats)
         
         # size has to manually set if different dir than benchmark_examples
         if args.directory == "num_robots_benchmark":
@@ -60,16 +61,15 @@ def main():
         # density = int(stats[3][-2])
         # num = int(stats[4].split(".")[0].split("x")[1])
 
-
-        signal.signal(signal.SIGALRM, handler)
-        # timeout after 5 min
-        signal.alarm(5*60)
+        if sys.platform != "win32":
+            signal.signal(signal.SIGALRM, handler)
+            # timeout after 5 min
+            signal.alarm(5*60)
 
         try:
-
             # horizon is size*2
             if args.greedy:
-                grun(["-b","-g",f"-hz={size*2}",f"{file}",f"{output}"])
+                grun(["-b",f"-hz={size*2}",f"{file}",f"{output}"])
             else:
                 run(["-b",f"-hz={size*2}",f"{file}",f"{output}"])
             print(f"{file} successfully run.")
@@ -81,8 +81,8 @@ def main():
             with open(output, 'a', encoding='utf-8', newline='') as f:
                     writer = csv.writer(f)
                     writer.writerow([f'{file}'])                
-
-        signal.alarm(0) # cancel timer if function returns before timeout
+        if sys.platform != "win32":
+            signal.alarm(0) # cancel timer if function returns before timeout
 
 if __name__ == "__main__":
     main()
