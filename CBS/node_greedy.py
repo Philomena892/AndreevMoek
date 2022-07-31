d65fa6471e34536b2d792d43228dab0a38b42b75
Philomena Moek <76845802+Philomena892@users.noreply.github.com>
2022-07-31 17:52:16 +0200

    added comments + documentation, removed functions -> imported from node.py

--- CBS/node_greedy.py
+++ CBS/node_greedy.py
@@ -6,35 +6,48 @@ import argparse
 from clingo.symbol import Function
 import sys
 import csv
+from node import Node, read_file, benchmark
 
 from dataclasses import dataclass, field
 from typing import Any
 
 @dataclass(order=True)
 class PrioritizedItem:
+    '''
+    Wrapper class needed for priority queue 
+    to only rank by cost and not by the node.
+    '''
     priority: int
     item: Any=field(compare=False)
 
-class Node():
-    def __init__(self):
-        # calculate from passed arguments
-        self.constraints = []      # list of all constraints
-        self.problem = ""                   # string of model
-        self.cost = 0
-        self.depth = 0
-
-# TODO think about how to make this more efficient
-
 def make_string(list):
+    '''
+    Converts a list of clingo symbols (clingo.symbol.Symbol)
+    to a string.
+    '''
     string = ""
     for elem in list:
+        # do not keep first_conflict of last iteration
         if elem.name == "first_conflict":
-            #print("first conflict found")
             continue
+        # append current atom in string format
         string += str(elem) + ". "
     return string
 
 def make_problem(input, horizon):
+    '''
+    Solves a problem using the clingo python API.
+
+        Parameters:  
+            input (str):    concatenated strings of the problem 
+                            (including the instance and the asp file)
+            horizon (int):  maximum makespan the solution is allowed to have
+
+        Returns:
+            new_problem (list of clingo.symbol.Symbol):
+                            list of symbols that are in the model 
+            cost (int):     number of conflicts in the model
+    '''
     cost = 0
     new_problem = []
     ctl = clingo.Control([f"-c horizon={horizon}"])
@@ -43,6 +56,8 @@ def make_problem(input, horizon):
     ctl.ground([("base", [])])
     with ctl.solve(yield_=True) as handle:
         try:
+            # get the last model in the iterator because 
+            # of the minimize statement
             my_iter = iter(handle)
             model = None
             for model in my_iter:
@@ -52,9 +67,11 @@ def make_problem(input, horizon):
             else:
                 print("model is None")
                 raise StopIteration
+        # no model was found = unsolvable
         except StopIteration:
             raise
     
+    # calculate cost as number of conflicts
     while new_problem[0].name == "conflict":
         cost = cost + 1
         new_problem.pop(0)
@@ -62,21 +79,39 @@ def make_problem(input, horizon):
     return cost, new_problem
 
 def get_children(parent, first_conflict, inits, low_level, shows, horizon):
+    '''
+    Creates and returns the two children of the passed node "parent".
+    
+        Parameters:
+            parent (node):      a parent node
+            first_conflict (list of clingo.symbol.Symbol): 
+                                the atom "first_conflict" which has the format
+                                first_conflict(Robot1, Robot2, Coordinates1, Coordinates2, Timestep)
+            inits (str):        init atoms
+            low_level (str):    low-level asp search file
+            shows (str):        string of asp #show 
+            horizon (int):      maximum makespan the solution is allowed to have
+        
+        Returns:
+            left_child (node):  node that contains one added constraint compared to parent
+            right_child (node): node that contains one added constraint compared to parent
+            error_num (int):    shows if a child (and which) hit a StopIteration Exception
+                        0 -> no problem
+                        1 -> problem with left child
+                        2 -> problem with right child
+                        3 -> problem with both
+    '''
+    
     # make constraint(Robot,Coordinates,Timestep) 
     # first_conflict has format first_conflict(Robot1, Robot2, Coordinates1, Coordinates2, Timestep)
     left_constraint = Function("constraint", [first_conflict[0], first_conflict[2], first_conflict[4]], True)
     right_constraint = Function("constraint", [first_conflict[1], first_conflict[3], first_conflict[4]], True)
     
-    # shows if a child (and which) hit a StopIteration Exception
-    # 0 -> no problem
-    # 1 -> problem with left child
-    # 2 -> problem with right child
-    # 3 -> problem with both
     error_num = 0
     
     problem = make_string(parent.problem)
 
-    #left child
+    # initiate left child
     left_child = Node()
     left_child.constraints = parent.constraints.copy()
     left_child.constraints.append(left_constraint)
@@ -89,7 +124,7 @@ def get_children(parent, first_conflict, inits, low_level, shows, horizon):
     except StopIteration:
         error_num = 1
 
-    #right child
+    # initiate right child
     right_child = Node()
     right_child.constraints = parent.constraints.copy()
     right_child.constraints.append(right_constraint)
@@ -102,42 +137,25 @@ def get_children(parent, first_conflict, inits, low_level, shows, horizon):
         if error_num == 1: error_num = 3
         else: error_num = 2
     
-    #print(f"left constraints: {lconstraints} \n right constraints: {rconstraints}")
-
     return left_child, right_child, error_num
-
-def read_file(file_name):
-    try:
-        with open(file_name) as file_object:
-            lines = file_object.readlines()
-    except FileNotFoundError:
-        sys.exit(f"The file {file_name} could not be found.")
-    file_string = ''
-    for line in lines: 
-        file_string += line.rstrip() 
-        file_string += " " 
-    return file_string
-
-def benchmark(name, current, node_counter, timer, last_move=0, move_sum=0):
-
-    print("\n---BENCHMARK----------------------------------") 
-    runtime = perf_counter() - timer          
-    print(f"running time: {runtime:0.4f}")
-    print(f"amount of nodes explored: {node_counter}")
-    print(f"length of path through the tree: {current.depth}")
-    last_move = 0
-    move_sum = 0
-    for elem in current.problem:
-        if elem.name == "occurs":
-            if elem.arguments[2].number > last_move: # find the time of the last move
-                last_move = elem.arguments[2].number
-            move_sum += 1
-    print(f"timesteps taken until completion of problem: {last_move}")
-    print(f"amount of moves made in total: {move_sum}\n")
-    return [name, runtime, node_counter, current.depth, last_move, move_sum]
 
 
 def main(raw_args=None):
+    '''
+    Runs greedy-CBS on the specified input file. Returns a suboptimal solution.
+
+    usage: node_greedy.py [-h] [-b] -hz HORIZON input [benchmark_file]
+
+    positional arguments:
+        input                 ASP file containing robot plans
+        benchmark_file        By default benchmarked values are saved in bm_output.csv. Specify a file here, if you want to append them to it instead.
+
+    optional arguments:
+        -h, --help            show this help message and exit
+        -b, --benchmark       output benchmarked values to the command line
+        -hz HORIZON, --horizon HORIZON
+                                maximum makespan the solution is allowed to have
+    '''
 
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="ASP file containing robot plans")
@@ -147,6 +165,7 @@ def main(raw_args=None):
     args = parser.parse_args(raw_args)
     print(f"vars(args): {vars(args)}")
 
+    # save values for benchmarking
     node_counter = 1
     if args.benchmark:
         timer = perf_counter()
@@ -169,8 +188,8 @@ def main(raw_args=None):
     root = Node()
     root.depth = 0
 
+    # save init atoms in extra string so they are not always reloaded
     inits = make_string(make_problem(problem_file + " #show. #show init/2.", args.horizon)[1])
-    #print("\ninits:" + inits + "\n")
 
     root.cost, root.problem = make_problem(asp_file + problem_file + shows, args.horizon)
 
@@ -183,14 +202,14 @@ def main(raw_args=None):
         node_counter += 1
 
         # check whether there is a first conflict
-        #print(f"\n\ncurrent.problem: {make_string(current.problem)}\n\n")
-        #print(f"cost of current.problem: {current.cost}")
         first_conflict = (list(current.problem))[0]
-        #print("first_conflict: " + str(first_conflict))
         if first_conflict.name != "first_conflict":
+            # solution was found
             print("no first_conflict found")
 
             if args.benchmark:
+                # if a file for benchmark results is specified (args.benchmark_file != "bm_output.csv")
+                # append to that file, otherwise overwrite bm_output.csv
                 new_file = False if os.path.exists(args.benchmark_file) else True
 
                 mode = 'a'
@@ -204,7 +223,7 @@ def main(raw_args=None):
                     writer.writerow(benchmark(args.input, current, node_counter, timer))
                 
             
-            # write to output file
+            # write solution to output file
             mode = 'w' if os.path.exists("output.lp") else 'a'
             with open("output.lp", mode, encoding='utf-8') as file:
                 file.write(inits)
@@ -212,14 +231,18 @@ def main(raw_args=None):
                     file.write(str(elem) + ". ")
 
             return True
+        
+        #create children of current node
         l_child, r_child, num = get_children(current, first_conflict.arguments, inits, asp_file, shows, args.horizon)
 
+        # error handling + enqueueing the children
         if num == 3: break
         if num != 1:
             queue.put(PrioritizedItem(l_child.cost, l_child))
         if num != 2:
             queue.put(PrioritizedItem(r_child.cost, r_child))
     
+    # if no nodes left in queue
     print("\nNo solution found!")
     if args.benchmark:
         new_file = False if os.path.exists(args.benchmark_file) else True
