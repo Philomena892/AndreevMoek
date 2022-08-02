7dfdb105bf8556254eae5abc4971e0500a6fa390
Philomena Moek <76845802+Philomena892@users.noreply.github.com>
2022-08-02 15:29:33 +0200

    rename to make_inc_examples.py

--- CBS/make_inc_examples.py
+++ CBS/make_inc_examples.py
@@ -1,16 +1,24 @@
+'''
+Creates a folder with example instances containing MIN_ROBOTS to 
+MAX_ROBOTS robots. For example: The instance 3robs.lp consists of
+2robs.lp with one added robot and its paths.
+'''
+
 import gen
 from gen import make_instance, gen_coo, make_paths, to_asprilo_format
 import os
 import sys
 
-
+# size of the example
 SIZE = 7
+# minimum and maximum amount of robots
 MIN_ROBOTS = 2
-maxRobots = 19     # density 40 = 19 robots on size 7
+MAX_ROBOTS = 19     # density 40 = 19 robots on size 7
 
 DIR = "num_robots_benchmark"
 os.system(f"mkdir {DIR}")
 
+# create the example with MIN_ROBOTS robots
 cooShelves, cooRobs = gen_coo(SIZE, MIN_ROBOTS)
 inits = make_instance(SIZE, MIN_ROBOTS, cooShelves, cooRobs)
 moves = to_asprilo_format(make_paths(cooShelves, cooRobs))
@@ -20,10 +28,9 @@ path = os.path.join(DIR, f"{MIN_ROBOTS}robs.lp")
 with open(path, "a", encoding='utf-8') as f:
     f.write(inits + moves)
 
+# create all other examples by adding one robot each time
+for n in range(MIN_ROBOTS+1, MAX_ROBOTS+1):
 
-for n in range(MIN_ROBOTS+1, maxRobots+1):
-
-    # generate one more distinct robot + shelf coordinates pair
     s, r = gen_coo(SIZE, 1)
     while (s[0] in cooShelves) or (r[0] in cooRobs):
         s, r = gen_coo(SIZE, 1)
